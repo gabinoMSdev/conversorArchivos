7e1d04ec33c693c777b2c215db97c83efecb8649
mjcontrerasp <mjcontrerasp01@educarex.es>
2025-03-26 10:58:08 +0100

    add menu

--- Conversor/src/Main.java
+++ Conversor/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("hola mundo bb");
+        Gestor gestor = new Gestor();
+        gestor.menu();
     }
 }
