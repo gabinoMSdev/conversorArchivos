acdb24359876316203bab2f0f17c75f121096296
mjcontrerasp <mjcontrerasp01@educarex.es>
2025-03-28 12:44:54 +0100

    JavaDoc Create and comment

--- Conversor/src/FileExtension.java
+++ Conversor/src/FileExtension.java
@@ -1,3 +1,21 @@
+/**
+ * <h1>Extensión de archivos</h1>
+ * 
+ * <p>
+ * Representa los formatos de archivo soportados por el sistema de conversión.
+ * </p>
+ * 
+ * <h2>Valores posibles</h2>
+ * <ul>
+ * <li>CSV</li>
+ * <li>JSON</li>
+ * <li>XML</li>
+ * <li>OTRO</li>
+ * </ul>
+ * 
+ * @see Conversor
+ * @see Gestor
+ */
 public enum FileExtension {
     CSV, JSON, XML, OTRO
 }
--- Conversor/src/Main.java
+++ Conversor/src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
+    /**
+     * <h2>Main principal de ejecución del programa</h2>
+     * @author Gabino y Manu
+     * @version v1.0
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         Gestor gestor = new Gestor();
         gestor.menu();
